9cdb3b00ae22b11db32adb96fa79c71823ba146b
jodhyyy <jodhyh71@gmail.com>
2023-10-06 19:46:44 +0700

    Selesai

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Task_1_AndroFund_Jodhy_A_Mobile_Morning"
+include(":app")
+ 
